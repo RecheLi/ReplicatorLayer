13a00a5e4c229bf23251b570a798bd8f9542ab00
Apple <Apple@AppledeiMac.local>
2016-09-13 16:16:16 +0800

    update

--- ReplicatorHUD/ViewController.m
+++ ReplicatorHUD/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ReplicatorView.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [ReplicatorView showReplicatorLoadingInView:self.view];
+}
+
+- (IBAction)show:(UIButton *)sender {
+    [ReplicatorView showReplicatorLoadingInView:self.view];
+}
+
+- (IBAction)hide:(UIButton *)sender {
+    [ReplicatorView disMissReplicatorLoadingInView:self.view];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
